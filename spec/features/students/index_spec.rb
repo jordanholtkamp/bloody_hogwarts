227d55597c545217b75eb0f6023323792ab19336
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-06 10:36:58 -0700

    Write avg age test

--- spec/features/students/index_spec.rb
+++ spec/features/students/index_spec.rb
@@ -6,6 +6,8 @@ describe 'As a visitor', type: :feature do
     before :each do
       @draco = Student.create!(name: "Draco Malfoy", age: 15, house: "Slytherin")
       @longbottom = Student.create!(name: "Neville Longbottom", age: 14, house: "Gryffindor")
+      @cedric = Student.create!(name: 'Cedric Diggory', age: 17, house: 'Ravenclaw')
+      @harry = Student.create!(name: 'Harry Potter', age: 15, house: 'Gryffindor')
     end
 
     it 'shows all the info for each student' do
@@ -18,6 +20,12 @@ describe 'As a visitor', type: :feature do
       expect(page).to have_content(@longbottom.name)
       expect(page).to have_content(@longbottom.age)
       expect(page).to have_content(@longbottom.house)
+    end
+
+    it 'shows the average age for all students' do
+      visit '/students'
+
+      expect(page).to have_content('Average Age: 15.25')
     end
   end
 end
