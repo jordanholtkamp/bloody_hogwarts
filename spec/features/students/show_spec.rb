817d5dee374853adf1329918369bd5beaab07ea8
Jordan Holtkamp <jordanholtkamp@gmail.com>
2019-12-06 10:02:43 -0700

    Write tests to show courses on student show page

---
+++ spec/features/students/show_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe 'As a visitor', type: :feature do
+  describe 'when I am on a student show page' do
+    it 'shows a list of their courses' do
+      longbottom = Student.create!(name: "Neville Longbottom", age: "14", house: "Gryffindor")
+
+      herbology = Course.create!(name: "Herbology")
+      dark_arts = Course.create!(name: "Defense against the dark arts")
+
+      herbology.students << longbottom
+      dark_arts.students << longbottom
+
+      visit "/students/#{longbottom.id}"
+
+      expect(page).to have_content('Herbology')
+      expect(page).to have_content('Defense against the dark arts')
+    end
+  end
+end
